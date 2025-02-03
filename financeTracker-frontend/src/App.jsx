5e54ac2491abdf909f3e0bfbd44763e71a212f5c
Jajohl <jajohl@utu.fi>
2025-02-03 17:58:13 +0200

    base for front and back

--- financeTracker-frontend/src/App.jsx
+++ financeTracker-frontend/src/App.jsx
@@ -1,10 +1,46 @@
 import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
-
-const App = () => (
-  <div>
-    <p>Hello world testi testi</p>
-  </div>
+const Frontpage = () => (
+  <div> <h2>Frontpage</h2> </div>
 )
+
+const Savings = () => (
+  <div> <h2>Savings</h2> </div>
+)
+
+const Graphs = () => (
+  <div> <h2>Graphs</h2> </div>
+)
+
+
+const App = () => {
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to="/">frontpage</Link>
+        <Link style={padding} to="/savings">savings</Link>  
+        <Link style={padding} to="/graphs">graphs</Link>
+      </div>
+
+      <Routes>
+        <Route path="/savings" element={<Savings />} />
+        <Route path="/graphs" element={<Graphs />} />
+        <Route path="/" element={<Frontpage />} />
+      </Routes>
+
+      <div>
+        <i>Stay on track with your monthly finances</i>
+      </div>
+    </Router>
+  )
+}
 
 export default App
